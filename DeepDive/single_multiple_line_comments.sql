e21b8767776b7f7605f2b72df3d741b4681a98a3
Ilir Sheraj <sherajilir@gmail.com>
2022-10-04 01:30:07 +0200

    Add files via upload

--- DeepDive/single_multiple_line_comments.sql
+++ DeepDive/single_multiple_line_comments.sql
@@ -8,3 +8,28 @@ and focus the filtering on a single person
 
 SELECT first_name, last_name FROM employees 
 WHERE first_name = 'Mayumi' AND last_name = 'Schueller';
+
+-- Get all Female Employees
+SELECT first_name, last_name FROM employees WHERE gender = 'F';
+
+-- Filter by multiple criteria
+SELECT first_name, last_name, hire_date FROM employees WHERE first_name = 'Georgi'
+AND last_name = 'Facello' AND hire_date = '1986-06-26' OR first_name = 'Bezalel' 
+AND last_name = 'Simmel';
+
+/*
+The previous query is different from the next one because of the AND and OR operators
+This is because of ORDER OF OPERATIONS (SQL will check certain things according to the way 
+we structure the filter)
+AND chains the next command to previous chain of events
+OR forgets everything until then and starts looking for something completely different
+It creates a sort of new filter
+*/
+SELECT first_name, last_name, hire_date FROM employees WHERE first_name = 'Georgi'
+OR first_name = 'Bezalel' AND last_name = 'Simmel' AND last_name = 'Simmel'
+AND hire_date = '1986-06-26';
+
+-- We can use paranthesis to give precedents to commands we want
+SELECT first_name, last_name, hire_date FROM employees 
+WHERE (first_name = 'Georgi'AND last_name = 'Facello' AND hire_date = '1986-06-26') 
+OR (first_name = 'Bezalel' AND last_name = 'Simmel');
