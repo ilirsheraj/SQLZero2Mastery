3baf92d269202530b4d5d760e28dec38bb2f19e7
Ilir Sheraj <sherajilir@gmail.com>
2022-10-04 02:37:24 +0200

    Add files via upload

---
+++ DeepDive/comparison_query_exercises.sql
@@ -0,0 +1,20 @@
+-- More exercises
+
+-- Question 1: How many female customers do we have from the state of Oregon (OR)?
+SELECT COUNT(customerid) FROM customers
+WHERE state = 'OR' AND gender = 'F';
+
+-- Who over the age of 44 has an income of 100 000 or more? (excluding 44)
+SELECT firstname, lastname, income FROM customers
+WHERE age > 44 AND income >= 100000;
+
+-- Who between the ages of 30 and 50 has an income less than 50 000?
+-- (include 30 and 50 in the results)
+
+SELECT firstname, lastname, age, income FROM customers
+WHERE (age >= 30 AND age <= 50) AND income < 50000;
+
+-- What is the average income between the ages of 20 and 50? (Excluding 20 and 50)
+
+SELECT AVG(income) FROM customers
+WHERE age > 20 AND age < 50;
