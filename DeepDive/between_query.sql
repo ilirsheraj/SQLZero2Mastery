5365df93505a1bdabeb463f43bc4861a90931a18
Ilir Sheraj <sherajilir@gmail.com>
2022-10-08 02:20:45 +0200

    Add files via upload

---
+++ DeepDive/between_query.sql
@@ -0,0 +1,20 @@
+/*
+Question 1
+-- Who between the ages of 30 and 50 has an income less than 50 000?
+-- (include 30 and 50 in the results)
+* Write your query here
+*/
+
+SELECT * FROM customers WHERE
+(age BETWEEN 30 AND 50)
+AND income < 50000;
+
+
+/*
+Question 2
+--What is the average income between the ages of 20 and 50? (Including 20 and 50)
+--Write your query here
+*/
+
+SELECT AVG(income) FROM customers 
+WHERE age BETWEEN 20 AND 50;
